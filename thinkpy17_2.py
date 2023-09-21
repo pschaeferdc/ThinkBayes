93e3e35b7932afcb94e2e759b863a79ea1d2789c
pschaeferdc <pschaeferdc@gmail.com>
2023-09-21 17:01:02 -0600

    ex17_4 done

--- thinkpy17_2.py
+++ thinkpy17_2.py
@@ -3,10 +3,16 @@ class Point(object):
     def __init__(self, x=0,y=0):
         self.x = x
         self.y = y
-    
-    #todo:  
+     
     def __str__(self):
         return'%0.2f, %2.2f' %(self.x, self.y)
+    
+    def __add__(self, other):
+        new_x = self.x + other.x
+        new_y = self.y + other.y
+        return'%0.2f, %2.2f' %(new_x, new_y)
+
 
 p1 = Point(.4, 4.4)
-print (p1)
+p2 = Point(.3, 3.3)
+print (p1 + p2)
